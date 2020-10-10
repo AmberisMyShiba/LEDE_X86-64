31f5b4c71405492993f4400022d7550b66713f77
TeF <tefiszx@gmail.com>
2020-10-10 11:01:38 +0800

    Replace argon-theme

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,3 +10,7 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+# Replace argon theme
+rm -rf package/lean/luci-theme-argon/
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
