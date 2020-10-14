ce6caf79c00b0d110b176150f5c5786550c44542
TeF <tefiszx@gmail.com>
2020-10-14 10:23:11 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,9 +11,10 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
-# Replace argon theme
-rm -rf package/lean/luci-theme-argon/
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+# Replace argon theme - no need because kenzok8 feeds prvides new argon_new theme
+#rm -rf package/lean/luci-theme-argon/
+#git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 
-# Add Extra packages
+# Add Extra packages - no need because kenzok8 feeds priveds all of extra packages like
+# adguardhome,smartdns,openclash etc
 git clone https://github.com/rufengsuixing/luci-app-adguardhome package/lean/luci-app-adguardhome
