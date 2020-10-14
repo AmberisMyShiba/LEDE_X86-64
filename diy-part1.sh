45a18f3b94f7f80be0327e0b096e44f079d85857
TeF <tefiszx@gmail.com>
2020-10-14 10:25:50 +0800

    No need for new feeds.conf.default uploaded
    all extra packages were provided form kenzok8 repository with new feeds
    like :
    ssr-plus,passwall,openclash,Adguarhome,smartdns etc

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,17 +9,17 @@
 #=============================================================
 
 # Uncomment a feed source
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
 # Add an openclash feed
-sed -i '$a src-git openclash https://github.com/vernesong/OpenClash' feeds.conf.default
+#sed -i '$a src-git openclash https://github.com/vernesong/OpenClash' feeds.conf.default
 
 # Add luci-themes feed
-sed -i '$a src-git darkmatter git://github.com/apollo-ng/luci-theme-darkmatter.git' feeds.conf.default
-sed -i '$a src-git rosy https://github.com/rosywrt/luci-theme-rosy.git;openwrt-18.06' feeds.conf.default
+#sed -i '$a src-git darkmatter git://github.com/apollo-ng/luci-theme-darkmatter.git' feeds.conf.default
+#sed -i '$a src-git rosy https://github.com/rosywrt/luci-theme-rosy.git;openwrt-18.06' feeds.conf.default
 
 #Install Extra packages
 #sed -i '$a src-git argon https://github.com/jerrykuku/luci-theme-argon.git;18.06' feeds.conf.defautl
