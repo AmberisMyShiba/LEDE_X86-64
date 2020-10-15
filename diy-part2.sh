ee299fc7d990b365a3aa81c259e1ffd30eeb01d7
TeF <tefiszx@gmail.com>
2020-10-15 12:38:30 +0800

    repatch kenso8's naiveproxy patchfiles

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,4 +17,8 @@
 
 # Add Extra packages - no need because kenzok8 feeds priveds all of extra packages like
 # adguardhome,smartdns,openclash etc
-git clone https://github.com/rufengsuixing/luci-app-adguardhome package/lean/luci-app-adguardhome
+#git clone https://github.com/rufengsuixing/luci-app-adguardhome package/lean/luci-app-adguardhome
+
+# repatch kenso8's naiveproxy patchfiles
+sed -i 's/if (is_linux) {/if (is_linux || is_chromeos) {/' package/feeds/kenzo/naiveproxy/patches/100-build-add-OpenWrt-staging-libraries.patch
+sed -i 's/@@ -746,7 +746,6/@@ -751,7 +751,6/' package/feeds/kenzo/naiveproxy/patches/101-build-drop-useless-deps-simd_asm.patch
